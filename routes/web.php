6c73b9ebfa2cdb5d635528c7573b279c60c0385a
Lucky Santoso <lucky.sntso@gmail.com>
2024-10-20 20:40:22 +0700

    Add Seeder, N+1 problem, Redesign UI, Searching, Pagination

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => "Blog", 'posts' => Post::all() ]);
+    // dump(request('search'));
+    
+    return view('posts', ['title' => "Blog", 
+    'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -29,9 +32,16 @@ Route::get('/contact', function () {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
-    return view('posts', ['title'=> count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+    // $posts = $user->posts->load('category', 'author');
+
+    return view('posts', ['title'=> count($user->posts) . 
+    ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title'=> 'Articles in: ' . $category->name, 'posts' => $category->posts]);
+    
+    // $posts = $category->posts->load('category', 'author');
+
+    return view('posts', ['title'=> 'Articles in: ' . 
+    $category->name, 'posts' => $category->posts]);
 });
